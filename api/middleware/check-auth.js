9bbd550e6d509fa9bfc239608f50d77df5031c53
Joaquin-M2 <joaquin.mmol@gmail.com>
2023-11-16 01:18:23 +0100

    Send an error if trying to reach an endpoint that requires a jwt

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -5,7 +5,14 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(" ")[1];
     const decodedJwt = jwt.verify(token, process.env.JWT_KEY);
     req.userData = decodedJwt;
-    next();
+    if (Date.now() < new Date(req.userData.exp * 1000)) {
+      next();
+    } else {
+      return res.status(401).json({
+        message: "⛔ Expired token. Log in again ⛔",
+        expiredToken: true,
+      });
+    }
   } catch (error) {
     return res.status(401).json({
       message: "⛔ Unauthorized ⛔",
