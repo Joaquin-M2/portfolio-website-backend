f3ae6416b7ae90a5dac8e43af6aff2e67f780f49
Joaquin-M2 <joaquin.mmol@gmail.com>
2023-11-23 10:43:34 +0100

    Update "check-auth" middleware
    So users without the "admin" role cannot send a PATCH request to the user endpoint change user roles.

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -1,18 +1,27 @@
 const jwt = require("jsonwebtoken");
+const UserModel = require("../models/user");
 
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     const decodedJwt = jwt.verify(token, process.env.JWT_KEY);
     req.userData = decodedJwt;
-    if (Date.now() < new Date(req.userData.exp * 1000)) {
-      next();
-    } else {
-      return res.status(401).json({
-        message: "⛔ Expired token. Log in again ⛔",
-        expiredToken: true,
+
+    UserModel.find({ email: req.userData.email })
+      .exec()
+      .then((user) => {
+        if (user[0].role !== "admin" && req.body.role) {
+          delete req.body.role;
+        }
+        if (Date.now() < new Date(req.userData.exp * 1000)) {
+          next();
+        } else {
+          return res.status(401).json({
+            message: "⛔ Expired token. Log in again ⛔",
+            expiredToken: true,
+          });
+        }
       });
-    }
   } catch (error) {
     return res.status(401).json({
       message: "⛔ Unauthorized ⛔",
